3334ee2dee3e9cc7f3c267148fcc6357529ecd9b
Dave-Skinner <dave@thishe.re>
2020-04-20 11:35:28 +0100

    Input Updates 4

--- index.py
+++ index.py
@@ -4,11 +4,9 @@ from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
 import flask
-import base64
 
 from app import app
 import app_worldwide
-
 
 
 
