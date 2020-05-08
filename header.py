eef62e7c31661d2b8f87c032698e4d6db3a4a132
Dave-Skinner <dave@thishe.re>
2020-05-08 13:49:45 +0100

    Update header.py

--- header.py
+++ header.py
@@ -3,22 +3,33 @@ import dash_html_components as html
 import dash_core_components as dcc
 
 def getHeader(active):
-	header_logo = html.Div([html.A(className="header__logo",
-										   href='/')])
+	header_title = html.Div('''This app is built using various Covid-19 related data sources. 
+								The Cumulative Data page tracks cumulative Covid-19 deaths/cases and
+								extends predictions using the latest daily rate of change.
+								The Stringency Data page compares the OxCGRT Stringency Index (a measurement of 
+								Covid-19 policy responses around the world) to daily Covid-19 deaths/cases in each country. ''',className='header_title')
 	header_items = [
 		html.Li(
-			dcc.Link("Worldwide", href="/worldwide", className="header__link"),
+			dcc.Link("Cumulative Data", href="/worldwide", className="header__link"),
 			className="header__item {0}".format("is-active" if active == "worldwide" else "")
 		),
+		#html.Li(
+		#	dcc.Link("Google Mobility Data", href="/mobility", className="header__link"),
+		#	className="header__item {0}".format("is-active" if active == "mobility" else "")
+		#),
 		html.Li(
-			dcc.Link("Players", href="/players", className="header__link"),
-			className="header__item {0}".format("is-active" if active == "players" else "")
+			dcc.Link("Stringency Data", href="/stringency", className="header__link"),
+			className="header__item {0}".format("is-active" if active == "stringency" else "")
 		),
+		#html.Li(
+		#	dcc.Link("Density Data", href="/density", className="header__link"),
+		#	className="header__item {0}".format("is-active" if active == "density" else "")
+		#),
 	]
 	header_list = html.Ul(header_items, className="header__list")
 
 
 	return html.Div([
-		header_logo,
+		header_title,
 		header_list,
 	], className="header t-sans")
