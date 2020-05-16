11bf184f3ad4382a13212e5af0ef4281e98eb722
Dave-Skinner <dave@thishe.re>
2020-05-16 13:43:03 +0100

    Variable R0 slider 5

--- app_sir.py
+++ app_sir.py
@@ -647,7 +647,7 @@ def updateTotalDeathsTimeline(location,
 			prediction_r0 = r0_list[-1]+r0_shift
 
 			if len(bar_widths) < len(r0_list):
-				r0_list = r0_list.pop()
+				r0_list.pop()
 			bar_widths.append(prediction_bar_width)
 			bar_position.append(prediction_bar_position)
 			r0_list.append(prediction_r0)
